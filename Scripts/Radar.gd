d6483713e8f115d13061cb1766ff3b501119782f
Allexis Alvarico <44900809+AllexisAlvarico@users.noreply.github.com>
2022-07-20 11:30:00 +0100

    added the id of the planes

--- Scripts/Radar.gd
+++ Scripts/Radar.gd
@@ -5,19 +5,17 @@ var random = RandomNumberGenerator.new()
 var enemyscene = load("res://Prefabs/PingScene.tscn")
 onready var radar = get_node("RadarSprite")
 
-
-
 func _ready():
 	for _i in range(0,10):
 		var enemy = enemyscene.instance()
 		random.randomize()
-		var new_pos = Vector2(random.randf_range(258,833),random.randf_range(13,587))
+		var new_pos = Vector2(random.randf_range(-256,256),random.randf_range(-256,256))
 		if new_pos.distance_to(radar.position) > radarRadius:
 			var angle = asin(float(new_pos.x / new_pos.distance_to(radar.position)))
 			new_pos.x = radarRadius * sin(angle)
 			new_pos.y = radarRadius * cos(angle)
 		enemy.position = new_pos
-		data.setID(_i)
+		enemy.setID(_i)
 		add_child(enemy)
 		#print(_i, " Enemy position: ", new_pos)
 
