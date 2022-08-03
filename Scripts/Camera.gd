6ba53e44527a4f5ae7a55177e78d95c7b19bea85
Allexis Alvarico <44900809+AllexisAlvarico@users.noreply.github.com>
2022-08-03 09:35:56 +0100

    added a mouse camera movement

--- Scripts/Camera.gd
+++ Scripts/Camera.gd
@@ -11,15 +11,18 @@ var mouse_speed = 0.1
 #		Public Functions			#
 #####################################
 func _ready():
-	PlayerData.current_rotation = self.rotation_degrees.y
+
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 
-func _physics_process(delta):
-	if Input.is_action_pressed("ui_right"):
-		self.rotate_y(-mouse_speed)
-	if Input.is_action_pressed("ui_left"):
-		self.rotate_y(mouse_speed)
-	PlayerData.current_rotation = self.rotation_degrees.y
+func _input(event):
+	if event is InputEventMouseMotion:
+		var movement = event.relative
+		self.rotation.x -= deg2rad(movement.y * mouse_speed)
+		self.rotation.x = clamp(self.rotation.x,deg2rad(0),deg2rad(90))
+		rotation.y += -deg2rad(movement.x * mouse_speed)
+		PlayerData.current_rotation = self.rotation_degrees.y
+
 
 #####################################
 #			Signals					#
