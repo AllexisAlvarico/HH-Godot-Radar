7a6d6d5b2f69dbf8c270c4d5bb9c39f58fe7e2e1
Richard Fleming <richard.fleming164@gmail.com>
2022-08-10 15:31:50 +0100

    Added plane flight

--- Scripts/Movement.gd
+++ Scripts/Movement.gd
@@ -1,11 +1,16 @@
 extends Spatial
 var random = RandomNumberGenerator.new()
-var angle : float
+var velocity : Vector3
+var speed := 10.0
+onready var plane = get_child(0)
 
 func _ready():
 	random.randomize()
-	angle = random.randf_range(1,3)
+	speed = random.randf_range(5,15)
 
-
-func _process(delta):
-	rotate_y(angle * delta)
+func _physics_process(delta: float) -> void:
+   var tr =  plane.global_transform.looking_at(plane.get_child(0).get_child(0).transform.origin, Vector3.UP)
+   plane.global_transform = plane.global_transform.interpolate_with(tr, 0.1)
+   velocity = Vector3.ZERO
+   velocity += plane.transform.basis.x * speed
+   plane.move_and_collide(velocity * delta)
